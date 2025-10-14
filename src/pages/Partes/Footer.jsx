af21635e3146449f095e7f72a1fa8592433f2667
matias-vega-morales <Md.vega@duocuc.cl>
2025-10-13 23:34:51 -0300

    Primeros pasos web

--- src/pages/Partes/Footer.jsx
+++ src/pages/Partes/Footer.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 
+
 export const Footer = () => {
 	return (
-		<footer className="main-footer">
-			<div className="container footer-content">
-				<div className="footer-left">© 2025 Tienda Online. Todos los derechos reservados.</div>
+        <>
+            <footer className="main-footer">
+                <div className="container footer-content">
+                    <div className="footer-left">© 2025 Tienda Online. Todos los derechos reservados.</div>
 
-				<div className="footer-center">
-					<img src="/img/logo.png" alt="Logo Tienda Online" className="logo-img-footer" />
-					<br />
-					<a href="/contacto.html">Contacto</a> |
-					<a href="#">Instagram</a> |
-					<a href="#">Facebook</a>
-				</div>
+                    <div className="footer-center">
+                        <img src="/img/logo.png" alt="Logo Tienda Online" className="logo-img-footer" />
+                        <br />
+                        <a href="/contacto.html">Contacto</a> |
+                        <a href="#">Instagram</a> |
+                        <a href="#">Facebook</a>
+                    </div>
 
-				<div className="footer-right">
-					<form className="newsletter-form">
-						<input type="email" placeholder="Tu email" required />
-						<button type="submit">Suscribirse</button>
-					</form>
-				</div>
-			</div>
-		</footer>
+                    <div className="footer-right">
+                        <form className="newsletter-form">
+                            <input type="email" placeholder="Tu email" required />
+                            <button type="submit">Suscribirse</button>
+                        </form>
+                    </div>
+                </div>
+            </footer>
+        </>
 	)
 }
-
-export default Footer
 
